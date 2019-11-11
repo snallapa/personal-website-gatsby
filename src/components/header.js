736829359094198c0b7bb130a5197d462b4465c4
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-11-11 00:43:12 -0500

    component now

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,18 @@
 import React from "react"
 import links from "./links"
 import { Link } from "gatsby"
+import styles from "./header.module.css"
 export default () => (
-  <div className="header">
-    <h1>
-      <Link className="home" to="/">
+  <div className={styles.header}>
+    <h1 className={styles.name}>
+      <Link className={styles.home} to="/">
         Sahith Nallapareddy
       </Link>
     </h1>
-    <div className="links">
+    <div>
       {links.map((link, idx) => {
         return (
-          <a key={idx} className="nav" href={`${link.name}`}>
+          <a key={idx} className={styles.nav} href={`${link.name}`}>
             {link.name}
           </a>
         )
