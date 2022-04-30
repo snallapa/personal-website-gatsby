f4f0c689396067dc02bee7a3ba1c6468fc07ed52
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-04-30 19:24:29 -0400

    add discord functionless

---
+++ netlify/functions/discord.js
@@ -0,0 +1,15 @@
+import {
+    InteractionType,
+    InteractionResponseType
+  } from 'discord-interactions';
+
+exports.handler = async function(event, context) {
+    const { type, id, data } = JSON.parse(event.body);
+    if (type === InteractionType.PING) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ type: InteractionResponseType.PONG }),
+          };
+    }
+
+}
