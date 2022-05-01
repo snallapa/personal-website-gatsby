df702dc605af0af8b4e4f7d21e90e10c3c176110
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-04-30 23:18:23 -0400

    adding signature checking

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -1,9 +1,27 @@
 import {
     InteractionType,
-    InteractionResponseType
+    InteractionResponseType,
+    verifyKey
   } from 'discord-interactions';
 
+function VerifyDiscordRequest(clientKey) {
+  return function (event) {
+    const signature = event.headers['x-signature-ed25519'];
+    const timestamp = event.headers['x-signature-timestamp'];
+
+    const isValidRequest = verifyKey(event.body, signature, timestamp, clientKey);
+    return isValidRequest;
+  };
+}
+
+const verifier = VerifyDiscordRequest(process.env.PUBLIC_KEY);
+
 exports.handler = async function(event, context) {
+    if (verifier(event)) {
+        return {
+            statusCode: 401
+        }
+    }
     console.log(event)
     const { type, id, data } = JSON.parse(event.body);
     if (type === InteractionType.PING) {
