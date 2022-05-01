ac5c4909b54db02b1c71ad0b7ef18bd1b18f82c0
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-05-01 00:08:35 -0400

    adding madden exporter

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -3,14 +3,32 @@ import {
     InteractionResponseType,
     verifyKey
   } from 'discord-interactions';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDf9ZiTBWf-sWY007WsKktMPewcrs07CWw",
+    authDomain: "championslounge-f0f36.firebaseapp.com",
+    projectId: "championslounge-f0f36",
+    storageBucket: "championslounge-f0f36.appspot.com",
+    messagingSenderId: "163156624093",
+    appId: "1:163156624093:web:dfe860c8bb38a62b075134"
+};
+  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 
 function VerifyDiscordRequest(clientKey) {
   return function (event) {
     const signature = event.headers['x-signature-ed25519'];
     const timestamp = event.headers['x-signature-timestamp'];
-    console.log(`signature ${signature} and timestamp ${timestamp}`)
     const isValidRequest = verifyKey(event.body, signature, timestamp, clientKey);
-    console.log(`valid?? ${isValidRequest}`)
     return isValidRequest;
   };
 }
---
+++ netlify/functions/exporter.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDf9ZiTBWf-sWY007WsKktMPewcrs07CWw",
+    authDomain: "championslounge-f0f36.firebaseapp.com",
+    projectId: "championslounge-f0f36",
+    storageBucket: "championslounge-f0f36.appspot.com",
+    messagingSenderId: "163156624093",
+    appId: "1:163156624093:web:dfe860c8bb38a62b075134"
+};
+  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+exports.handler = async function(event, context) {
+    console.log(event)
+}
