6d4d5b22bb9604217d1048f70972e9f0b11893e4
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-07-02 12:05:33 -0400

    remove log

--- netlify/functions/exporter.js
+++ netlify/functions/exporter.js
@@ -36,7 +36,6 @@ exports.handler = async function(event, context) {
     if (currentPath == "leagueteams") {
         const { leagueTeamInfoList: teamsData } = JSON.parse(event.body)
         let teams = {}
-        console.log(teamsData);
         teamsData.forEach(t => teams[t.teamId] = {teamName: t.displayName, abbr: t.abbrName, username: t.userName, division: t.divName, cityName: t.cityName});
         try {
             
