ca8f79d93c32c25ac6906eb123f30b95055c9e59
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-07-02 10:51:36 -0400

    import

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -5,7 +5,7 @@ import {
   } from 'discord-interactions';
 import { initializeApp } from "firebase/app";
 
-import { getFirestore , doc, getDoc} from "firebase/firestore";
+import { getFirestore , doc, getDoc, setDoc} from "firebase/firestore";
 
 import fetch from 'node-fetch';
 
