3507587acc5e324fa9e04fe755d92ca15dd247b4
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-07-02 11:51:34 -0400

    add conference and division

--- netlify/functions/exporter.js
+++ netlify/functions/exporter.js
@@ -36,7 +36,7 @@ exports.handler = async function(event, context) {
     if (currentPath == "leagueteams") {
         const { leagueTeamInfoList: teamsData } = JSON.parse(event.body)
         let teams = {}
-        teamsData.forEach(t => teams[t.teamId] = {teamName: t.displayName, abbr: t.abbrName, username: t.userName});
+        teamsData.forEach(t => teams[t.teamId] = {teamName: t.displayName, abbr: t.abbrName, username: t.userName, conference: t.conferenceName, division: t.divName});
         try {
             
             await setDoc(doc(db, "leagues", league), {
