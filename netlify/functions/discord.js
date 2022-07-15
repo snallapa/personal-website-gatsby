a95ef5faf47c045c955f87e56ec431f9220a53be
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-07-15 18:56:33 -0400

    if message is gone dont try updating it

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -53,7 +53,7 @@ async function DiscordRequest(endpoint, options) {
     // throw API errors
     if (!res.ok) {
         const data = await res.json();
-        console.log(res.status);
+        console.log(res);
         throw new Error(JSON.stringify(data));
     }
     // return original response
