69ee0d721c37ccee6adbdd1e0822eede67ce55d3
Sahith Nallapareddy <sahith.reddy@gmail.com>
2022-10-02 23:31:19 -0400

    add cors

--- src/pages/snallabot.js
+++ src/pages/snallabot.js
@@ -18,144 +18,169 @@ const CONSOLE_MAP = {
     'MADDEN_23SDA' : 'sda'
 }
 
-export default ({ location }) => {
-    console.log(location);
-    const params = new URLSearchParams(location.search);
+const CORS = `https://thawing-atoll-70735.herokuapp.com/`;
+
+
+
+export default () => {
     const code = params.get("code");
     const [state, setState] = useState({
-        loading: true,
-        blazeSession: ""
+        loading: "",
+        blazeSession: "",
+        code: ""
     });
 
-    useEffect(() => {
-        const login = async () => {
-            const res1 = await fetch("https://accounts.ea.com/connect/token", {
-                method: 'POST', 
-                headers : {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-                    'Accept': 'application/json;charset=utf-8'
-                },
-                body: `client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`
-            });
-            const res1Json = await res1.json();
-            const access_token = res1Json["access_token"];
+    const login = async () => {
+        const res1 = await fetch(`${CORS}https://accounts.ea.com/connect/token`, {
+            method: 'POST', 
+            headers : {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+                'Accept': 'application/json;charset=utf-8'
+            },
+            body: `client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`
+        });
+        const res1Json = await res1.json();
+        const access_token = res1Json["access_token"];
 
-            const res2 = await fetch(`https://accounts.ea.com/connect/tokeninfo?access_token=${access_token}`, {
-                headers: {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'Accept': 'application/text'
-                }
-            });
-            const res2Json = await res2.json();
-            const pid = res2Json["pid_id"];
+        const res2 = await fetch(`${CORS}https://accounts.ea.com/connect/tokeninfo?access_token=${access_token}`, {
+            headers: {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/json;charset=utf-8',
+                'Accept': 'application/text'
+            }
+        });
+        const res2Json = await res2.json();
+        const pid = res2Json["pid_id"];
 
-            const res3 = await fetch(`https://gateway.ea.com/proxy/identity/pids/${pid}/personas?access_token=${access_token}&status=ACTIVE`, {
-                headers: {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'Accept': 'application/json;charset=utf-8',
-                    'X-Expand-Results': 'true'
-                }
-            });
-            const res3Json = await res3.json();
+        const res3 = await fetch(`${CORS}https://gateway.ea.com/proxy/identity/pids/${pid}/personas?access_token=${access_token}&status=ACTIVE`, {
+            headers: {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/json;charset=utf-8',
+                'Accept': 'application/json;charset=utf-8',
+                'X-Expand-Results': 'true'
+            }
+        });
+        const res3Json = await res3.json();
 
-            const personaId = res3Json["personas"]["persona"].filter(p => p.showPersona === "EVERYONE")[0].personaId;
+        const personaId = res3Json["personas"]["persona"].filter(p => p.showPersona === "EVERYONE")[0].personaId;
 
-            const res4 = await fetch(`https://gateway.ea.com/proxy/identity/pids/${pid}/entitlements?status=ACTIVE`, {
-                headers: {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'Allow-Content-Type':  'application/json',
-                    'Accept': 'application/json',
-                    'X-Expand-Results': 'true',
-                    'Authorization': `Bearer ${access_token}`
+        const res4 = await fetch(`${CORS}https://gateway.ea.com/proxy/identity/pids/${pid}/entitlements?status=ACTIVE`, {
+            headers: {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/json;charset=utf-8',
+                'Allow-Content-Type':  'application/json',
+                'Accept': 'application/json',
+                'X-Expand-Results': 'true',
+                'Authorization': `Bearer ${access_token}`
 
-                }
-            });
-            const res4Json = await res4.json();
+            }
+        });
+        const res4Json = await res4.json();
 
-            const system = res4Json["entitlements"]["entitlement"].filter(ent => ent["groupName"].includes("MADDEN_23") && ent["entitlementTag"] === "ONLINE_ACCESS")[0]["groupName"];
+        const system = res4Json["entitlements"]["entitlement"].filter(ent => ent["groupName"].includes("MADDEN_23") && ent["entitlementTag"] === "ONLINE_ACCESS")[0]["groupName"];
 
-            const res5 = await fetch(`https://accounts.ea.com/connect/auth?response_type=code&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&access_token=${access_token}&persona_id=${personaId}&client_id=MaddenCompanionApp19`, {
-                headers: {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'Accept': 'application/text',
-                    'Access-Control-Expose-Headers': 'Location'
-                }
-            });
+        const res5 = await fetch(`${CORS}https://accounts.ea.com/connect/auth?response_type=code&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&access_token=${access_token}&persona_id=${personaId}&client_id=MaddenCompanionApp19`, {
+            headers: {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/json;charset=utf-8',
+                'Accept': 'application/text',
+                'Access-Control-Expose-Headers': 'Location'
+            }
+        });
 
-            const locationUrl = res5.headers.get('Location');
-            const code2 = new URLSearchParams(locationUrl.replace("http://127.0.0.1/success", "")).get("code");
+        const locationUrl = res5.headers.get('Location');
+        const code2 = new URLSearchParams(locationUrl.replace("http://127.0.0.1/success", "")).get("code");
 
-            const res6 = await fetch("https://accounts.ea.com/connect/token", {
-                method: 'POST', 
-                headers : {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-                    'Accept': 'application/json;charset=utf-8'
-                },
-                body: `client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code2}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`
-            });
-            const res6Json = await res6.json();
-            const access_token2 = res6Json["access_token"];
+        const res6 = await fetch(`${CORS}https://accounts.ea.com/connect/token`, {
+            method: 'POST', 
+            headers : {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+                'Accept': 'application/json;charset=utf-8'
+            },
+            body: `client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code2}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`
+        });
+        const res6Json = await res6.json();
+        const access_token2 = res6Json["access_token"];
 
 
-            const res7 = await fetch(`https://accounts.ea.com/connect/tokeninfo?access_token=${access_token2}`, {
-                headers: {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'Accept': 'application/text'
-                }
-            });
-            const res7Json = await res7.json();
-            const pidType = res7Json["pid_type"]; // related to system (PS3)
+        const res7 = await fetch(`${CORS}https://accounts.ea.com/connect/tokeninfo?access_token=${access_token2}`, {
+            headers: {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/json;charset=utf-8',
+                'Accept': 'application/text'
+            }
+        });
+        const res7Json = await res7.json();
+        const pidType = res7Json["pid_type"]; // related to system (PS3)
 
-            const res8 = await fetch(`https://accounts.ea.com/connect/auth?response_type=code&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&access_token=${access_token2}&persona_id=${personaId}&client_id=${SYSTEM_MAP[system]}`, {
-                headers: {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'Accept': 'application/text',
-                    'Access-Control-Expose-Headers': 'Location'
-                }
-            });
+        const res8 = await fetch(`${CORS}https://accounts.ea.com/connect/auth?response_type=code&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&access_token=${access_token2}&persona_id=${personaId}&client_id=${SYSTEM_MAP[system]}`, {
+            headers: {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/json;charset=utf-8',
+                'Accept': 'application/text',
+                'Access-Control-Expose-Headers': 'Location'
+            }
+        });
 
-            const locationUrl2 = res8.headers.get('Location');
-            const code3 = new URLSearchParams(locationUrl2.replace("http://127.0.0.1/success", "")).get("code");
+        const locationUrl2 = res8.headers.get('Location');
+        const code3 = new URLSearchParams(locationUrl2.replace("http://127.0.0.1/success", "")).get("code");
 
-            const res9 = await fetch(`https://wal2.tools.gos.bio-iad.ea.com/wal/authentication/login`, {
-                method: 'POST',
-                headers: {
-                    'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Accept-Encoding': 'gzip',
-                    'X-BLAZE-VOID-RESP': 'XML',
-                    'X-Application-Key': 'MADDEN-MCA',
-                    'X-BLAZE-ID': `madden-2023-${CONSOLE_MAP[system]}-mca`
-                },
-                body: JSON.stringify({
-                    'authCode': code3
-                })
-            });
-            const res9Json = await res9.json();
-            const blazeSession = res9Json['userLoginInfo']['sessionKey'];
+        const res9 = await fetch(`${CORS}https://wal2.tools.gos.bio-iad.ea.com/wal/authentication/login`, {
+            method: 'POST',
+            headers: {
+                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Accept-Encoding': 'gzip',
+                'X-BLAZE-VOID-RESP': 'XML',
+                'X-Application-Key': 'MADDEN-MCA',
+                'X-BLAZE-ID': `madden-2023-${CONSOLE_MAP[system]}-mca`
+            },
+            body: JSON.stringify({
+                'authCode': code3
+            })
+        });
+        const res9Json = await res9.json();
+        const blazeSession = res9Json['userLoginInfo']['sessionKey'];
 
-            console.log(blazeSession);
-            setState({
-                ...state,
-                loading: false,
-                blazeSession: blazeSession
-            });
-        }
+        console.log(blazeSession);
+        setState({
+            ...state,
+            loading: false,
+            blazeSession: blazeSession
+        });
+    }
 
-        login().catch(console.error);
-    }, [state.loading, state.blazeSession]);
+    const handleChange = (e) => {
+        setState({
+            ...state,
+            code: e.target.value
+        });
+    }
+
+    const handleClick = (e) => {
+        setState({
+            ...state,
+            loading: "LOADING"
+        });
+        await login();
+    }
 
     return (
-        <div> {state.loading ? "Loading" : state.blazeSession} </div>
+        <div>
+            <div>Login to EA <a href="https://accounts.ea.com/connect/auth?response_type=code&hide_create=true&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&client_id=MaddenCompanionApp19" target="_blank">here</a>. Once you are logged in it will have an error/blank page at the end. That is correct</div>
+            <div>
+                <label>
+                    Enter the URL of the page. It should start with 127.0.0.1:
+                    <input type="text" value={state.code} onChange={handleChange} />
+                </label>
+                <button onClick={handleClick}>Retrieve Leagues</button>
+            </div>
+            <div> {state.loading === "LOADING" && <h2> Loading </h2> } </div>
+        </div>
+        
     )
 
 }
