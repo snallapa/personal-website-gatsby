bbfb05d5a8c89d4bccd0f48364473c23738a78be
Sahith Nallapareddy <sahith.reddy@gmail.com>
2023-07-16 21:56:43 -0400

    add verify key

--- modules/utils.js
+++ modules/utils.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch"
-import { InteractionResponseType } from "discord-interactions"
+import { InteractionResponseType, verifyKey } from "discord-interactions"
 
 export async function DiscordRequest(
   endpoint,
