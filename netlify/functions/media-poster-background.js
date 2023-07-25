3e05097757f33f64763e9a72453c8fef9be5c015
Sahith Nallapareddy <sahith.reddy@gmail.com>
2023-07-25 02:31:08 -0400

    move week num

--- netlify/functions/media-poster-background.js
+++ netlify/functions/media-poster-background.js
@@ -129,7 +129,10 @@ exports.handler = async function (event, context) {
   const weeks = league.schedules.reg
 
   const { weekNum, game } = findWeekAndGame(weeks, Number(scheduleId))
-  const leagueWeek = await getMediaWeek(guild_id, weekNum)
+  const leagueWeek = await getMediaWeek(
+    guild_id,
+    Number(weekNum.replace("week", ""))
+  )
   const { awayScore, homeScore, awayTeamId, homeTeamId } = game
   const homeTeamStats = leagueWeek["team-stats"][homeTeamId]
   const awayTeamStats = leagueWeek["team-stats"][awayTeamId]
