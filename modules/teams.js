970832fc7644ab39806648e82311056bbb127906
Sahith Nallapareddy <sahith.reddy@gmail.com>
2023-09-13 03:05:07 -0400

    add reset team (#26)

--- modules/teams.js
+++ modules/teams.js
@@ -1,4 +1,4 @@
-import { doc, setDoc } from "firebase/firestore"
+import { doc, setDoc, updateDoc, deleteField } from "firebase/firestore"
 import { respond } from "./utils.js"
 import { DiscordRequestProd } from "./utils.js"
 import { getLeague, db } from "./firebase-db.js"
@@ -289,8 +289,38 @@ async function handleOpen(guild_id, command, member) {
   }
 }
 
+async function handleReset(guild_id, command, member) {
+  let league
+  try {
+    league = await getLeague(guild_id)
+  } catch (e) {
+    console.error(e)
+    return respond(e.message)
+  }
+  const messageId = league.commands.teams.message
+  const channelId = league.commands.teams.channel
+  const res = await DiscordRequestProd(
+    `channels/${channelId}/messages/${messageId}`,
+    {
+      method: "DELETE",
+    }
+  )
+
+  await updateDoc(doc(db, "leagues", guild_id), {
+    teams: deleteField(),
+    commands: {
+      teams: {
+        message: deleteField(),
+      },
+    },
+  })
+
+  return respond("team reset")
+}
+
 export const teamHandler = {
   configure: handleConfigure,
   assign: handleAssign,
   open: handleOpen,
+  reset: handleReset,
 }
