d0be8c6fde7da42121304e33f55713be6ca5f5cb
Sahith Nallapareddy <sahith.reddy@gmail.com>
2023-10-08 02:16:29 -0400

    persona picking

--- netlify/functions/ea-persona-picker.js
+++ netlify/functions/ea-persona-picker.js
@@ -1,37 +1,49 @@
 import fetch from "node-fetch"
 
-const SYSTEM_MAP = {
-  MADDEN_23XONE: "MADDEN_23_XONE_BLZ_SERVER",
-  MADDEN_23PS4: "MADDEN_23_PS4_BLZ_SERVER",
-  MADDEN_23PC: "MADDEN_23_PC_BLZ_SERVER",
-  MADDEN_23PS5: "MADDEN_23_PS5_BLZ_SERVER",
-  MADDEN_23XBSX: "MADDEN_23_XBSX_BLZ_SERVER",
-  MADDEN_23SDA: "MADDEN_23_SDA_BLZ_SERVER",
+const SYSTEM_MAP = (a) => ({
+  xone: `MADDEN_${a}_XONE_BLZ_SERVER`,
+  ps4: `MADDEN_${a}_PS4_BLZ_SERVER`,
+  pc: `MADDEN_${a}_PC_BLZ_SERVER`,
+  ps5: `MADDEN_${a}_PS5_BLZ_SERVER`,
+  xbsx: `MADDEN_${a}_XBSX_BLZ_SERVER`,
+  stadia: `MADDEN_${a}_SDA_BLZ_SERVER`,
+})
+
+const VALID_ENTITLEMENTS = (a) => ({
+  xone: `MADDEN_${a}XONE`,
+  ps4: `MADDEN_${a}PS4`,
+  pc: `MADDEN_${a}PC`,
+  ps5: `MADDEN_${a}PS5`,
+  xbsx: `MADDEN_${a}XBSX`,
+  stadia: `MADDEN_${a}SDA`,
+})
+
+const namespaces = {
+  xbox: "XBOX",
+  ps3: "PSN",
+  cem_ea_id: "EA Account",
+  stadia: "Stadia",
 }
 
-const CONSOLE_MAP = {
-  MADDEN_23XONE: "xone",
-  MADDEN_23PS4: "ps4",
-  MADDEN_23PC: "pc",
-  MADDEN_23PS5: "ps5",
-  MADDEN_23XBSX: "xbsx",
-  MADDEN_23SDA: "sda",
-}
-
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
   const body = JSON.parse(event.body)
   const code = body.code
+  console.log(code)
   const res1 = await fetch(`https://accounts.ea.com/connect/token`, {
     method: "POST",
     headers: {
-      "User-Agent": "ProtoHttp 1.3/DS 15.1.2.2.0 (Android)",
-      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-      Accept: "application/json;charset=utf-8",
+      "Accept-Charset": "UTF-8",
+      "User-Agent":
+        "Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone_x86_64 Build/TE1A.220922.031)",
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "Accept-Encoding": "gzip",
     },
-    body: `client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`,
+    body: `authentication_source=317239&client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`,
   })
   const res1Json = await res1.json()
   const access_token = res1Json["access_token"]
+  const refresh_token = res1Json["refresh_token"]
+  const expiresIn = res1Json["expires_in"]
 
   console.log(res1Json)
 
@@ -39,9 +51,11 @@ exports.handler = async function(event, context) {
     `https://accounts.ea.com/connect/tokeninfo?access_token=${access_token}`,
     {
       headers: {
-        "User-Agent": "ProtoHttp 1.3/DS 15.1.2.2.0 (Android)",
-        "Content-Type": "application/json;charset=utf-8",
-        Accept: "application/text",
+        "Accept-Charset": "UTF-8",
+        "X-Include-Deviceid": "true",
+        "User-Agent":
+          "Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone_x86_64 Build/TE1A.220922.031)",
+        "Accept-Encoding": "gzip",
       },
     }
   )
@@ -51,41 +65,49 @@ exports.handler = async function(event, context) {
   console.log(res2Json)
 
   const res3 = await fetch(
-    `https://gateway.ea.com/proxy/identity/pids/${pid}/personas?access_token=${access_token}&status=ACTIVE`,
+    `https://gateway.ea.com/proxy/identity/pids/${pid}/entitlements/?status=ACTIVE`,
     {
       headers: {
-        "User-Agent": "ProtoHttp 1.3/DS 15.1.2.2.0 (Android)",
-        "Content-Type": "application/json;charset=utf-8",
-        Accept: "application/json;charset=utf-8",
+        "User-Agent":
+          "Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone_x86_64 Build/TE1A.220922.031)",
+        "Accept-Charset": "UFT-8",
         "X-Expand-Results": "true",
+        "Accept-Encoding": "gzip",
+        Authorization: `Bearer ${access_token}`,
       },
     }
   )
   const res3Json = await res3.json()
 
-  console.log(res3Json["personas"]["persona"])
+  console.log(res3Json["entitlements"]["entitlement"])
 
-  const personaId = res3Json["personas"]["persona"].filter(
-    p => p.showPersona === "EVERYONE"
-  )[0].personaId
+  const pidUri = res3Json["entitlements"]["entitlement"].filter(
+    (p) =>
+      p.entitlementTag === "ONLINE_ACCESS" &&
+      Object.values(VALID_ENTITLEMENTS("24")).includes(p.groupName)
+  )[0].pidUri
 
   const res4 = await fetch(
-    `https://gateway.ea.com/proxy/identity/pids/${pid}/entitlements?status=ACTIVE`,
+    `https://gateway.ea.com/proxy/identity${pidUri}/personas?status=ACTIVE&access_token=${access_token}`,
     {
       headers: {
-        "User-Agent": "ProtoHttp 1.3/DS 15.1.2.2.0 (Android)",
-        "Content-Type": "application/json;charset=utf-8",
-        "Allow-Content-Type": "application/json",
-        Accept: "application/json",
+        "Acccept-Charset": "UTF-8",
         "X-Expand-Results": "true",
-        Authorization: `Bearer ${access_token}`,
+        "User-Agent":
+          "Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone_x86_64 Build/TE1A.220922.031)",
+        "Accept-Encoding": "gzip",
       },
     }
   )
   const res4Json = await res4.json()
+  return {
+    statusCode: 200,
+    body: JSON.stringify(res4Json),
+  }
 
+  /*
   const system = res4Json["entitlements"]["entitlement"].filter(
-    ent =>
+    (ent) =>
       ent["groupName"].includes("MADDEN_23") &&
       ent["entitlementTag"] === "ONLINE_ACCESS"
   )[0]["groupName"]
@@ -102,7 +124,7 @@ exports.handler = async function(event, context) {
       },
     }
   )
-    .then(res5 => {
+    .then((res5) => {
       return res5.headers.get("Location")
     })
     .catch(console.warn)
@@ -148,10 +170,11 @@ exports.handler = async function(event, context) {
         "Access-Control-Expose-Headers": "Location",
       },
     }
-  ).then(res8 => {
-    return res8.headers.get("Location")
-  })
-  .catch(console.warn)
+  )
+    .then((res8) => {
+      return res8.headers.get("Location")
+    })
+    .catch(console.warn)
 
   const code3 = new URLSearchParams(
     locationUrl2.replace("http://127.0.0.1/success", "")
@@ -178,9 +201,5 @@ exports.handler = async function(event, context) {
   const res9Json = await res9.json()
   const blazeSession = res9Json["userLoginInfo"]["sessionKey"]
 
-  console.log(blazeSession)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ blazeSession }),
-  }
+  console.log(blazeSession)*/
 }
--- src/pages/snallabot.js
+++ src/pages/snallabot.js
@@ -1,187 +1,100 @@
 import React, { useState, useEffect } from "react"
+import fetch from "node-fetch"
 
-const SYSTEM_MAP = {
-    'MADDEN_23XONE': 'MADDEN_23_XONE_BLZ_SERVER',
-    'MADDEN_23PS4' : 'MADDEN_23_PS4_BLZ_SERVER',
-    'MADDEN_23PC'  : 'MADDEN_23_PC_BLZ_SERVER',
-    'MADDEN_23PS5' : 'MADDEN_23_PS5_BLZ_SERVER',
-    'MADDEN_23XBSX': 'MADDEN_23_XBSX_BLZ_SERVER',
-    'MADDEN_23SDA' : 'MADDEN_23_SDA_BLZ_SERVER'
+const namespaces = {
+  xbox: "XBOX",
+  ps3: "PSN",
+  cem_ea_id: "EA Account",
+  stadia: "Stadia",
 }
-
-const CONSOLE_MAP = {
-    'MADDEN_23XONE': 'xone',
-    'MADDEN_23PS4' : 'ps4',
-    'MADDEN_23PC'  : 'pc',
-    'MADDEN_23PS5' : 'ps5',
-    'MADDEN_23XBSX': 'xbsx',
-    'MADDEN_23SDA' : 'sda'
-}
-
-const CORS = `https://thawing-atoll-70735.herokuapp.com/`;
-
-
 
 export default () => {
-    const [state, setState] = useState({
-        loading: "",
-        blazeSession: "",
-        code: ""
-    });
+  const [state, setState] = useState({
+    loginState: "EA_LOGIN",
+    code: "",
+    personas: {},
+    selectedPersona: "",
+  })
 
-    const login = async (code) => {
-        const res1 = await fetch(`${CORS}https://accounts.ea.com/connect/token`, {
-            method: 'POST', 
-            headers : {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-                'Accept': 'application/json;charset=utf-8',
-                'Access-Control-Allow-Origin': 'https://nallapareddy.com'
-            },
-            body: `client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`
-        });
-        const res1Json = await res1.json();
-        const access_token = res1Json["access_token"];
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      code: e.target.value,
+    })
+  }
 
-        const res2 = await fetch(`${CORS}https://accounts.ea.com/connect/tokeninfo?access_token=${access_token}`, {
-            headers: {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/json;charset=utf-8',
-                'Accept': 'application/text'
-            }
-        });
-        const res2Json = await res2.json();
-        const pid = res2Json["pid_id"];
-
-        const res3 = await fetch(`${CORS}https://gateway.ea.com/proxy/identity/pids/${pid}/personas?access_token=${access_token}&status=ACTIVE`, {
-            headers: {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/json;charset=utf-8',
-                'Accept': 'application/json;charset=utf-8',
-                'X-Expand-Results': 'true'
-            }
-        });
-        const res3Json = await res3.json();
-
-        const personaId = res3Json["personas"]["persona"].filter(p => p.showPersona === "EVERYONE")[0].personaId;
-
-        const res4 = await fetch(`${CORS}https://gateway.ea.com/proxy/identity/pids/${pid}/entitlements?status=ACTIVE`, {
-            headers: {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/json;charset=utf-8',
-                'Allow-Content-Type':  'application/json',
-                'Accept': 'application/json',
-                'X-Expand-Results': 'true',
-                'Authorization': `Bearer ${access_token}`
-
-            }
-        });
-        const res4Json = await res4.json();
-
-        const system = res4Json["entitlements"]["entitlement"].filter(ent => ent["groupName"].includes("MADDEN_23") && ent["entitlementTag"] === "ONLINE_ACCESS")[0]["groupName"];
-
-        const res5 = await fetch(`${CORS}https://accounts.ea.com/connect/auth?response_type=code&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&access_token=${access_token}&persona_id=${personaId}&client_id=MaddenCompanionApp19`, {
-            headers: {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/json;charset=utf-8',
-                'Accept': 'application/text',
-                'Access-Control-Expose-Headers': 'Location'
-            }
-        });
-
-        const locationUrl = res5.headers.get('Location');
-        const code2 = new URLSearchParams(locationUrl.replace("http://127.0.0.1/success", "")).get("code");
-
-        const res6 = await fetch(`${CORS}https://accounts.ea.com/connect/token`, {
-            method: 'POST', 
-            headers : {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-                'Accept': 'application/json;charset=utf-8'
-            },
-            body: `client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&grant_type=authorization_code&code=${code2}&redirect_uri=http://127.0.0.1/success&release_type=prod&client_id=MaddenCompanionApp19`
-        });
-        const res6Json = await res6.json();
-        const access_token2 = res6Json["access_token"];
-
-
-        const res7 = await fetch(`${CORS}https://accounts.ea.com/connect/tokeninfo?access_token=${access_token2}`, {
-            headers: {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/json;charset=utf-8',
-                'Accept': 'application/text'
-            }
-        });
-        const res7Json = await res7.json();
-        const pidType = res7Json["pid_type"]; // related to system (PS3)
-
-        const res8 = await fetch(`${CORS}https://accounts.ea.com/connect/auth?response_type=code&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&access_token=${access_token2}&persona_id=${personaId}&client_id=${SYSTEM_MAP[system]}`, {
-            headers: {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/json;charset=utf-8',
-                'Accept': 'application/text',
-                'Access-Control-Expose-Headers': 'Location'
-            }
-        });
-
-        const locationUrl2 = res8.headers.get('Location');
-        const code3 = new URLSearchParams(locationUrl2.replace("http://127.0.0.1/success", "")).get("code");
-
-        const res9 = await fetch(`${CORS}https://wal2.tools.gos.bio-iad.ea.com/wal/authentication/login`, {
-            method: 'POST',
-            headers: {
-                'User-Agent': 'ProtoHttp 1.3/DS 15.1.2.2.0 (Android)',
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Accept-Encoding': 'gzip',
-                'X-BLAZE-VOID-RESP': 'XML',
-                'X-Application-Key': 'MADDEN-MCA',
-                'X-BLAZE-ID': `madden-2023-${CONSOLE_MAP[system]}-mca`
-            },
-            body: JSON.stringify({
-                'authCode': code3
-            })
-        });
-        const res9Json = await res9.json();
-        const blazeSession = res9Json['userLoginInfo']['sessionKey'];
-
-        console.log(blazeSession);
-        setState({
-            ...state,
-            loading: false,
-            blazeSession: blazeSession
-        });
-    }
-
-    const handleChange = (e) => {
-        setState({
-            ...state,
-            code: e.target.value
-        });
-    }
-
-    const handleClick = (e) => {
-        setState({
-            ...state,
-            loading: "LOADING"
-        });
-        const parsedCode = state.code.replace("http://127.0.0.1/success?code", "");
-        login(parsedCode);
-    }
-
-    return (
-        <div>
-            <div>Login to EA <a href="https://accounts.ea.com/connect/auth?response_type=code&hide_create=true&redirect_uri=http://127.0.0.1/success&machineProfileKey=1d6830c75f0f5a26&release_type=prod&client_id=MaddenCompanionApp19" target="_blank">here</a>. Once you are logged in it will have an error/blank page at the end. That is correct</div>
-            <div>
-                <label>
-                    Enter the URL of the page. It should start with 127.0.0.1:
-                    <input type="text" value={state.code} onChange={handleChange} />
-                </label>
-                <button onClick={handleClick}>Retrieve Leagues</button>
-            </div>
-            <div> {state.loading === "LOADING" && <h2> Loading </h2> } </div>
-        </div>
-        
+  async function choosePersona(code) {
+    const res = await fetch(
+      "http://localhost:8888/.netlify/functions/ea-persona-picker",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          code: code,
+        }),
+      }
     )
+    const personas = await res.json()
+    const personaList = personas.personas.persona
+    setState({
+      ...state,
+      loginState: "CHOOSE_PERSONA",
+      personas: personaList,
+      selectedPersona: personaList[0].personaId,
+    })
+  }
 
+  async function handleClick(e) {
+    const parsedCode = state.code.replace("http://127.0.0.1/success?code=", "")
+    await choosePersona(parsedCode)
+  }
+
+  async function selectPersona(e) {}
+
+  switch (state.loginState) {
+    case "EA_LOGIN":
+      return (
+        <div>
+          <div>
+            <a
+              href="https://accounts.ea.com/connect/auth?hide_create=true&release_type=prod&response_type=code&redirect_uri=http://127.0.0.1/success&client_id=MaddenCompanionApp19&machineProfileKey=MCA4b35d75Vm-MCA&authentication_source=317239"
+              target="_blank"
+            >
+              Login to EA
+            </a>
+            . Once you are logged in it will have an error/blank page at the
+            end. That is EXPECTED AND NORMAL!!
+          </div>
+          <div>
+            <label>
+              Enter the URL of the page. It should start with 127.0.0.1:
+              <input type="text" value={state.code} onChange={handleChange} />
+            </label>
+            <button onClick={handleClick}>Retrieve Leagues</button>
+          </div>
+          <div> {state.loading === "LOADING" && <h2> Loading </h2>} </div>
+        </div>
+      )
+    case "CHOOSE_PERSONA":
+      const options = state.personas.map((p) => (
+        <option value={p.personaId}>
+          {p.displayName} - {namespaces[p.namespaceName]}
+        </option>
+      ))
+      return (
+        <div>
+          <label>
+            Which EA account should we use?
+            <select
+              value={state.selectedPersona}
+              onChange={(e) =>
+                setState({ ...state, selectedPersona: e.target.value })
+              }
+            >
+              {options}
+            </select>
+          </label>
+          <button onClick={selectPersona}>Submit</button>
+        </div>
+      )
+  }
 }
