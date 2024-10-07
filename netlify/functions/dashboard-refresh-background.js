4eac1eb7c0bfb5e5758cb63e5cefb17a6c629171
Sahith Nallapareddy <sahith.reddy@gmail.com>
2024-10-07 15:44:00 -0400

    add background task

---
+++ netlify/functions/dashboard-refresh-background.js
@@ -0,0 +1,21 @@
+import { getAllDashboardLeagues } from "../../modules/firebase-db.js"
+
+exports.handler = async function (event, context) {
+    const leagues = await getAllDashboardLeagues()
+    console.log("updating " + leagues.length + " leagues")
+    for (let l of leagues) {
+	const res = await fetch(`https://snallabot.herokuapp.com/${l.data().guild_id}/export`, {
+          method: "POST",
+          body: JSON.stringify({
+            week: 102,
+            stage: -1,
+            auto: false,
+          }),
+        });
+	if (!res.ok) {
+	    const e = await res.text();
+	    console.error("Could not refresh league " + l.guildId + " error: " + e);
+	}
+	await new Promise(r => setTimeout(r, 3000));
+    }
+}
--- netlify/functions/dashboard-refresh.js
+++ netlify/functions/dashboard-refresh.js
@@ -1,25 +1,12 @@
-import { getAllDashboardLeagues } from "../../modules/firebase-db.js"
+
 
 exports.handler = async function (event, context) {
-    const leagues = await getAllDashboardLeagues()
-    console.log("will update leagues: " + leagues.map(l => l.guildId))
-    // await Promise.all(leagues.map(async l => {
-    // 	const res = await fetch(`https://snallabot.herokuapp.com/${l.guild_id}/export`, {
-    //       method: "POST",
-    //       body: JSON.stringify({
-    //         week: 102,
-    //         stage: -1,
-    //         auto: false,
-    //       }),
-    //     });
-    // 	if (!res.ok) {
-    // 	    const e = await res.text();
-    // 	    console.error("Could not refresh league " + l.guildId + " error: " + e);
-    // 	    return false;
-    // 	} else {
-    // 	    return true;
-    // 	}
-    // }));
+  await fetch(
+    "https://nallapareddy.com/.netlify/functions/dashboard-refresh-background",
+    {
+      method: "POST"
+    }
+  )
     return {
 	statusCode: 200,
     };
